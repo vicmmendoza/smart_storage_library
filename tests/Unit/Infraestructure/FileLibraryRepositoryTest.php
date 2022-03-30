e24052f817a995676fe9ae0ba0ab3318d1ce5dda
Victor Mendoza <victormendoza@animum3d.com>
2022-03-30 19:52:20 +0200

    Test unitarios FileLibraryRepository

---
+++ tests/Unit/Infraestructure/FileLibraryRepositoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Unit\Infraestructure;
+
+use PHPUnit\Framework\TestCase;
+
+final class FileLibraryRepositoryTest extends TestCase
+{
+
+    /** @test */
+    public function it_should_save_a_library(): void
+    {
+
+    }
+
+    /** @test */
+    public function it_should_search_a_library(): void
+    {
+        
+    }
+
+    /** @test */
+    public function it_should_not_return_a_non_existing_library(): void
+    {
+ 
+    }
+
+}
